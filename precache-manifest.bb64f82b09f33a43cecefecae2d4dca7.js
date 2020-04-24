b22b04a38ba3297033f7c53e529a4f5bd9eef4da
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-04-24 14:00:37 +0200

    Updates

---
+++ precache-manifest.bb64f82b09f33a43cecefecae2d4dca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af00d014b3d9db9e90bcb431f3a1972d",
+    "url": "/reactive-shop/index.html"
+  },
+  {
+    "revision": "53c4a39545fc9226d93a",
+    "url": "/reactive-shop/static/css/main.ae1169e6.chunk.css"
+  },
+  {
+    "revision": "4efbf4415456de7e8831",
+    "url": "/reactive-shop/static/js/2.4680e564.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactive-shop/static/js/2.4680e564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c4a39545fc9226d93a",
+    "url": "/reactive-shop/static/js/main.2378313b.chunk.js"
+  },
+  {
+    "revision": "39ec7b3591a5983d2641",
+    "url": "/reactive-shop/static/js/runtime-main.9f62fa0e.js"
+  }
+]);
