8fabad8e3aa05fc96e3b5cc10714f88dccc8ad99
RafalSzczuka <53195389+RafalSzczuka@users.noreply.github.com>
2020-04-28 18:02:57 +0200

    Updates

--- precache-manifest.2d74849ce35e696e1a1f2adfdbfdb52d.js
+++ precache-manifest.2d74849ce35e696e1a1f2adfdbfdb52d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "879586cfcaf2aef2ffbb45064657919e",
+    "revision": "caa360235058fed866dd677f39f8c5b6",
     "url": "/reactive-shop/index.html"
   },
   {
-    "revision": "71a9cf01b5cccf16ad29",
+    "revision": "3ed6249b1781f20c63de",
     "url": "/reactive-shop/static/css/main.ae1169e6.chunk.css"
   },
   {
-    "revision": "353522975a7bf9cefb95",
-    "url": "/reactive-shop/static/js/2.667f8529.chunk.js"
+    "revision": "f21c86cd95cb31bdabc8",
+    "url": "/reactive-shop/static/js/2.23d00f50.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/reactive-shop/static/js/2.667f8529.chunk.js.LICENSE.txt"
+    "url": "/reactive-shop/static/js/2.23d00f50.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "71a9cf01b5cccf16ad29",
-    "url": "/reactive-shop/static/js/main.d1d92e18.chunk.js"
+    "revision": "3ed6249b1781f20c63de",
+    "url": "/reactive-shop/static/js/main.a4a2c953.chunk.js"
   },
   {
     "revision": "39ec7b3591a5983d2641",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactive-shop/precache-manifest.86f58622581d469df1149eea36b8fed8.js"
+  "/reactive-shop/precache-manifest.2d74849ce35e696e1a1f2adfdbfdb52d.js"
 );
 
 self.addEventListener('message', (event) => {
